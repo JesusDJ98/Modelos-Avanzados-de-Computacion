bd5dcfbbf00ede3539e9483dcd9d2b208d8721ec
JesusDelgado <jesus.delgado742@alu.uhu.es>
2021-02-21 21:26:12 +0100

    Limpieza

--- PracticasMAC/9.integral_ManuelJesus.scala
+++
@@ -1,8 +0,0 @@
-def integralMJ(f: Double => Double, x: Double, y: Double, z: Double, salida: Double = 0): Double = {
-	if(x+z <= y) integralMJ(f, x+z, y, z, salida + z*f(x))
-	else salida
-}
-/Salida:
-scala> integral(math.pow(_,2),1,10,0.5)
-val res116: Double = 308.625
-*/
